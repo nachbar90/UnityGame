b0026d4016da4292de2b05322caf2c6b2589407f
nachbar90 <tomaszsiudak90@gmail.com>
2020-04-26 14:21:39 +0200

    Bullet scripts, physics material and bounciness from the walls

---
+++ ZanyBattleTactics/Assets/Script/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private int _hits = 1;
+    public int MyProperty { get; set; }
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    public int Hits
+    {
+        get
+        {
+            return _hits;
+        }
+        set
+        {
+            _hits = value;
+        }
+    }
+
+    public void DecreaseBulletLife()
+    {
+        _hits -= 1;
+    }
+
+    public IEnumerator InvokeBulletExplosionAnimationAndDestroyObject(GameObject gameObject)
+    {
+        animator.SetTrigger("BulletExplosion");
+        yield return new WaitForSeconds(0.05f);
+        Destroy(gameObject);
+    }
+
+}
--- ZanyBattleTactics/Assets/Script/LowerWall.cs
+++ ZanyBattleTactics/Assets/Script/LowerWall.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class LowerWall : MonoBehaviour
 {
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        var bullet = collision.collider.gameObject;
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        var bulletGameObject = collision.collider.gameObject;
+        var bullet = bulletGameObject.GetComponent<Bullet>();
+        Rigidbody2D rb = bulletGameObject.GetComponent<Rigidbody2D>();
+
+        if (bullet.Hits == 0)
+        {
+            rb.velocity = Vector2.zero;
+            rb.freezeRotation = true;
+            rb.gravityScale = 0;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            rb.angularVelocity = 0f;
+            rb.simulated = false; 
+            // none of any of this method stops bullet from bouncing;
+            StartCoroutine(bullet.InvokeBulletExplosionAnimationAndDestroyObject(bulletGameObject));        
+        }
+
+        bullet.DecreaseBulletLife();
         var currentRotation = rb.transform.eulerAngles.z;
-        //Debug.Log("rotation " + currentRotation);
         rb.SetRotation(currentRotation + Random.Range(80, 110));
     }
 }
