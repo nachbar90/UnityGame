e3c303d54d05564494afeb36504b20ac6783aedc
nachbar90 <tomaszsiudak90@gmail.com>
2020-04-27 20:45:33 +0200

    Enemy sprite + basic moving enemy script

---
+++ ZanyBattleTactics/Assets/Script/FlyingEnemyPath.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyPath : MonoBehaviour
+{
+    [SerializeField] List<Transform> points;
+    [SerializeField] float speed = 4f;
+    int i = 0;
+
+    void Start()
+    {
+        transform.position =  points[i].transform.position;
+    }
+
+    void Update()
+    {
+        if (i < points.Count)
+        {
+            var target = points[i].transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * speed);
+
+            if (transform.position == target)
+            {
+                i++;
+            }
+                
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
